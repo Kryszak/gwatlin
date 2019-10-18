0c9966aed89837627ee13c5799b4eb7e6af31017
Kryszak <prajskrzysztof@gmail.com>
2019-10-18 20:54:03 +0200

    public api documentation

--- src/main/kotlin/com/kryszak/gwatlin/api/model/achievement/group/AchievementGroup.kt
+++ src/main/kotlin/com/kryszak/gwatlin/api/model/achievement/group/AchievementGroup.kt
@@ -1,5 +1,8 @@
 package com.kryszak.gwatlin.api.model.achievement.group
 
+/**
+ * Data model for achievement group object
+ */
 data class AchievementGroup(val id: String,
                             val name: String,
                             val description: String,
