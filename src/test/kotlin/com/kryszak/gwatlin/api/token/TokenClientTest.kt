6aff87a6b1083822f2ba9a53742e860dcbff97d0
Kryszak <prajskrzysztof@gmail.com>
2023-04-28 15:43:03 +0200

    #15 migrate TokenClientSpec to TokenClientTest

---
+++ src/test/kotlin/com/kryszak/gwatlin/api/token/TokenClientTest.kt
@@ -0,0 +1,36 @@
+package com.kryszak.gwatlin.api.token
+
+import com.kryszak.gwatlin.config.WiremockTestKt
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+
+internal class TokenClientTest : WiremockTestKt() {
+
+    private val apiKey = "1234"
+
+    private val tokenClient = GWTokenClient(apiKey)
+
+    init {
+        should("Get token info") {
+            // given
+            stubAuthResponse("/tokeninfo", "/responses/token/token.json", apiKey)
+
+            // when
+            val tokenInfo = tokenClient.getTokenInfo()
+
+            // then
+            assertSoftly(tokenInfo) {
+                id shouldBe "ABCDE02B-8888-FEBA-1234-DE98765C7DEF"
+                name shouldBe "My API Key"
+                permissions shouldContainExactly listOf(
+                    "account",
+                    "characters",
+                    "tradingpost",
+                    "unlocks",
+                    "build"
+                )
+            }
+        }
+    }
+}
