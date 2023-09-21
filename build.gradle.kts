a378d65fd32df986967c729f66e35a9b016876f2
Kryszak <prajskrzysztof@gmail.com>
2023-09-21 12:31:25 +0200

    Add configuration for maven central artifact upload, remove releasing to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,152 @@ plugins {
     val kotlinVersion = "1.9.10"
     groovy
     jacoco
+    signing
     id("maven-publish")
     id("org.jetbrains.dokka") version "1.9.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+}
+
+project.group = "io.github.kryszak"
+project.version = "1.8"
+
+object Meta {
+    const val desc = "Guild Wars 2 API client"
+    const val license = "MiT"
+    const val githubRepo = "Kryszak/gwatlin"
+    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+}
+
+tasks {
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.required.set(true)
+            html.required.set(false)
+        }
+    }
+    test {
+        useJUnitPlatform()
+        finalizedBy(jacocoTestReport)
+    }
+    build {
+        finalizedBy(dokkaHtml)
+    }
+    withType<DokkaTask>().configureEach {
+        dokkaSourceSets {
+            named("main") {
+                moduleName.set("gwatlin")
+                includes.from("Module.md")
+                perPackageOption {
+                    matchingRegex.set(".*api.*")
+                    suppress.set(false)
+                }
+            }
+            configureEach {
+                perPackageOption {
+                    matchingRegex.set(".*")
+                    suppress.set(true)
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions
+            .getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set(project.name)
+                description.set(Meta.desc)
+                url.set("https://github.com/${Meta.githubRepo}")
+                licenses {
+                    license {
+                        name.set(Meta.license)
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("kryszak")
+                        name.set("Krzysztof Prajs")
+                    }
+                }
+                scm {
+                    url.set(
+                        "https://github.com/${Meta.githubRepo}.git"
+                    )
+                    connection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                }
+                issueManagement {
+                    url.set("https://github.com/${Meta.githubRepo}/issues")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri(Meta.release))
+            snapshotRepositoryUrl.set(uri(Meta.snapshot))
+            val ossrhUsername = providers
+                .environmentVariable("OSSRH_USERNAME")
+            val ossrhPassword = providers
+                .environmentVariable("OSSRH_PASSWORD")
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            }
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
 }
 
 val fuelVersion = "2.3.1"
@@ -17,76 +159,6 @@ val logbackVersion = "1.4.11"
 val kotestVersion = "5.7.2"
 val kotestWiremockExtensionVersion = "2.0.1"
 val kotlinWiremockDslVersion = "2.0.2"
-
-project.group = "io.github.kryszak"
-project.version = "1.8"
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-java {
-    withJavadocJar()
-}
-
-jacoco {
-    toolVersion = "0.8.9"
-}
-
-tasks.withType<DokkaTask>().configureEach {
-    dokkaSourceSets {
-        named("main") {
-            moduleName.set("gwatlin")
-            includes.from("Module.md")
-            perPackageOption {
-                matchingRegex.set(".*api.*")
-                suppress.set(false)
-            }
-        }
-        configureEach {
-            perPackageOption {
-                matchingRegex.set(".*")
-                suppress.set(true)
-            }
-        }
-    }
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        html.required.set(false)
-    }
-}
-
-tasks.test {
-    useJUnitPlatform()
-    finalizedBy(tasks.jacocoTestReport)
-}
-
-tasks.build {
-    finalizedBy(tasks.dokkaHtml)
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/kryszak/gwatlin")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     // http
