5bcfbf59858f1fe0308953ef30308c5e76c711a6
Northburns <northburns@gmail.com>
2024-03-04 12:10:30 +0200

    cleanup

--- src/test/kotlin/io/github/kryszak/gwatlin/http/serializers/SerialNameDelegateTest.kt
+++ src/test/kotlin/io/github/kryszak/gwatlin/http/serializers/SerialNameDelegateTest.kt
@@ -5,19 +5,23 @@ import io.github.kryszak.gwatlin.api.guild.model.log.GuildLogInvited
 import io.github.kryszak.gwatlin.api.items.model.item.ArmorItem
 import io.github.kryszak.gwatlin.api.items.model.item.ItemRarity
 import io.github.kryszak.gwatlin.api.items.model.item.ItemType
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
-class SerialNameDelegateTest : FunSpec({
-    test("Item->Armor serialname") {
+/**
+ * Ensure the [SerialNameDelegate] works as expected by checking a couple of selected classes.
+ * The values used in instance creation don't matter.
+ */
+internal class SerialNameDelegateTest : ShouldSpec({
+    should("Item->Armor serialname") {
         val armor = ArmorItem(0,"","","","",ItemRarity.FINE,0,0)
         armor.type shouldBe ItemType.ARMOR
     }
-    test("Fact->BuffConversion serialname") {
+    should("Fact->BuffConversion serialname") {
         val bc = BuffConversion(null,null,0.0f,"","")
         bc.type shouldBe "BuffConversion"
     }
-    test("GuildLog->GuildLogInvited serialname") {
+    should("GuildLog->GuildLogInvited serialname") {
         val gli = GuildLogInvited(0,"","","")
         gli.type shouldBe "invited"
     }
