91e17931444b3968498293f25f08d0fd5dcfd33c
Kryszak <13157331+Kryszak@users.noreply.github.com>
2024-12-10 13:57:49 +0100

    Format detect-ci.sh

--- scripts/detect-ci.sh
+++ scripts/detect-ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$CI" == "true" ]]; then
-        echo "Running in CI"
-    else
-        echo "This recipe should not be run locally. If you know what you are doing, set \$CI variable to 'true'."
-        exit 1
-    fi
+  echo "Running in CI"
+else
+  echo "This recipe should not be run locally. If you know what you are doing, set \$CI variable to 'true'."
+  exit 1
+fi
