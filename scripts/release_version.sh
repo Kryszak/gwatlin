1eacc5d4625f68bf43664dd804f342b29decee92
Kryszak <prajskrzysztof@gmail.com>
2024-12-10 13:28:30 +0100

    add check for last tag version before release creation

--- scripts/release_version.sh
+++ scripts/release_version.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 VERSION=v$(grep "project.version" build.gradle.kts | grep -Eo "[0-9].[0-9](.[0-9])?")
+LAST_VERSION=$(git describe --tags --abbrev=0)
+
+echo "Current version: $VERSION"
+echo "Last released version: $LAST_VERSION"
+
+if [[ "$VERSION" == "$LAST_VERSION" ]]; then
+  echo "Parsed project version is equal to last tagged version. Please increment project.version in build.gradle.kts first."
+  exit 1
+fi
 
 git tag "$VERSION"
 git push origin --tags
