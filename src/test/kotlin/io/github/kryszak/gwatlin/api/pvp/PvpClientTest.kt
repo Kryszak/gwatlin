420e9370ca036d4a5cc05043ebc4d82b7105e615
Kryszak <prajskrzysztof@gmail.com>
2024-12-11 15:31:57 +0100

    Fix pvp season serialization errors by adapting current pvp season schema

--- src/test/kotlin/io/github/kryszak/gwatlin/api/pvp/PvpClientTest.kt
+++ src/test/kotlin/io/github/kryszak/gwatlin/api/pvp/PvpClientTest.kt
@@ -1,6 +1,5 @@
 package io.github.kryszak.gwatlin.api.pvp
 
-import io.github.kryszak.gwatlin.api.ApiLanguage
 import io.github.kryszak.gwatlin.config.BaseWiremockTest
 import io.kotest.assertions.assertSoftly
 import io.kotest.matchers.collections.shouldHaveSize
@@ -62,12 +61,12 @@ internal class PvpClientTest : BaseWiremockTest() {
 
         should("Get pvp seasons") {
             // given
-            val ids = listOf("44B85826-B5ED-4890-8C77-82DDF9F2CF2B", "95D5B290-798A-421E-A919-1C2A75F74B72")
+            val ids = listOf("A54849B7-7DBD-4958-91EF-72E18CD659BA")
             val lang = io.github.kryszak.gwatlin.api.ApiLanguage.EN
 
             stubResponse(
-                "/pvp/seasons?ids=44B85826-B5ED-4890-8C77-82DDF9F2CF2B,95D5B290-798A-421E-A919-1C2A75F74B72",
-                "/responses/pvp/seasons.json", language = lang
+                "/pvp/seasons?ids=A54849B7-7DBD-4958-91EF-72E18CD659BA",
+                "/responses/pvp/season.json", language = lang
             )
 
             // when
@@ -75,35 +74,35 @@ internal class PvpClientTest : BaseWiremockTest() {
 
             // then
             assertSoftly(seasons[0]) {
-                id shouldBe "44B85826-B5ED-4890-8C77-82DDF9F2CF2B"
-                name shouldBe "PvP League Season One"
-                start shouldBe "2015-12-01T20:00:00.000Z"
-                end shouldBe "2016-01-28T01:00:00.000Z"
-                !active
+                id shouldBe "A54849B7-7DBD-4958-91EF-72E18CD659BA"
+                name shouldBe "PvP League Season Five"
+                start shouldBe "2016-12-13T20:00:00.000Z"
+                end shouldBe "2017-02-07T00:00:00.000Z"
+                active
                 assertSoftly(divisions[0]) {
-                    name shouldBe "Division 1: Amber"
+                    name shouldBe "Cerulean"
                     flags shouldHaveSize 0
-                    largeIcon shouldBe "https://render.guildwars2.com/file/02ED75461164551455297DA4955862552C2452BE/1313334.png"
-                    smallIcon shouldBe "https://render.guildwars2.com/file/6357FE56301B2F4AD1F309E62739B0110DA2452A/1313340.png"
-                    pipIcon shouldBe "https://render.guildwars2.com/file/47BDF237FF800552EDD69D28BC926031FC4B64A9/1313346.png"
+                    largeIcon.shouldBeNull()
+                    smallIcon.shouldBeNull()
+                    pipIcon shouldBe "https://render.guildwars2.com/file/DF4AE7C6B85711B5EF0A970851672FFE230FE61D/1614836.png"
                     assertSoftly(tiers[0]) {
-                        points shouldBe 5
+                        points shouldBe 20
                     }
                 }
-                assertSoftly(leaderboards.legendary) {
+                assertSoftly(leaderboards.ladder) {
                     assertSoftly(settings) {
                         name shouldBe ""
                         duration.shouldBeNull()
-                        scoring shouldBe "E6487336-4B5B-4BFA-9CFA-9FF232CAEF85"
+                        scoring shouldBe "0F86D504-63C2-4465-80AA-C278E1CB7800"
                         assertSoftly(tiers[0]) {
                             range shouldHaveSize 2
                         }
                     }
                     assertSoftly(scorings[0]) {
-                        id shouldBe "E6487336-4B5B-4BFA-9CFA-9FF232CAEF85"
+                        id shouldBe "0F86D504-63C2-4465-80AA-C278E1CB7800"
                         type shouldBe "Integer"
-                        description shouldBe "Current prestige rank. Prestige rank can be gained or lost by winning or losing ranked matches in the legendary division."
-                        name shouldBe "Prestige"
+                        description shouldBe "Effective rating that includes decay."
+                        name shouldBe "Rating"
                         ordering shouldBe "MoreIsBetter"
                     }
                 }
