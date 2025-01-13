8b6c904b2e90ccb2ebb446320db3d67b8f330a4e
Kryszak <prajskrzysztof@gmail.com>
2025-01-13 13:28:56 +0100

    164 implement get list of specializations method

--- src/main/kotlin/io/github/kryszak/gwatlin/api/gamemechanics/GWSpecializationClient.kt
+++ src/main/kotlin/io/github/kryszak/gwatlin/api/gamemechanics/GWSpecializationClient.kt
@@ -1,5 +1,6 @@
 package io.github.kryszak.gwatlin.api.gamemechanics
 
+import io.github.kryszak.gwatlin.api.ApiLanguage
 import io.github.kryszak.gwatlin.api.gamemechanics.model.specialization.Specialization
 import io.github.kryszak.gwatlin.clients.gamemechanics.SpecializationClient
 
@@ -25,7 +26,18 @@ class GWSpecializationClient {
      * @see io.github.kryszak.gwatlin.api.gamemechanics.model.specialization.Specialization
      */
     @JvmOverloads
-    fun getSpecialization(id: Int, language: io.github.kryszak.gwatlin.api.ApiLanguage? = null): Specialization {
+    fun getSpecialization(id: Int, language: ApiLanguage? = null): Specialization {
         return specializationClient.getSpecialization(id, language)
+    }
+
+    /**
+     * Retrieves list of specializations
+     * @param ids of specializations
+     * @param language of returned text (default=en)
+     * @see io.github.kryszak.gwatlin.api.gamemechanics.model.specialization.Specialization
+     */
+    @JvmOverloads
+    fun getSpecializations(ids: List<Int>, language: ApiLanguage? = null): List<Specialization> {
+        return specializationClient.getSpecializations(ids, language)
     }
 }
