53a0724362466f2a4f8d0f0d934926d52611ba93
Kryszak <13157331+Kryszak@users.noreply.github.com>
2025-01-17 10:22:55 +0100

    203 add missing methods for GWColorsClient (#211)

--- src/main/kotlin/io/github/kryszak/gwatlin/clients/gamemechanics/ColorClient.kt
+++ src/main/kotlin/io/github/kryszak/gwatlin/clients/gamemechanics/ColorClient.kt
@@ -1,5 +1,6 @@
 package io.github.kryszak.gwatlin.clients.gamemechanics
 
+import io.github.kryszak.gwatlin.api.ApiLanguage
 import io.github.kryszak.gwatlin.api.gamemechanics.model.color.DyeColor
 import io.github.kryszak.gwatlin.http.BaseHttpClient
 
@@ -7,7 +8,20 @@ internal class ColorClient : BaseHttpClient() {
 
     private val colorsEndpoint = "/colors"
 
-    fun getAllColors(language: io.github.kryszak.gwatlin.api.ApiLanguage?): List<DyeColor> {
+    fun getAllColors(language: ApiLanguage?): List<DyeColor> {
         return getRequest("$colorsEndpoint?ids=all", language)
+    }
+
+    fun getColorIds(): List<Int> {
+        return getRequest(colorsEndpoint)
+    }
+
+    fun getColor(id: Int, language: ApiLanguage?): DyeColor {
+        return getRequest("$colorsEndpoint/$id", language)
+    }
+
+    fun getColors(ids: List<Int>, language: ApiLanguage?): List<DyeColor> {
+        val params = ids.joinToString(",")
+        return getRequest("$colorsEndpoint?ids=$params", language)
     }
 }
--- src/test/kotlin/io/github/kryszak/gwatlin/api/gamemechanics/ColorClientTest.kt
+++ src/test/kotlin/io/github/kryszak/gwatlin/api/gamemechanics/ColorClientTest.kt
@@ -4,6 +4,7 @@ import io.github.kryszak.gwatlin.api.ApiLanguage
 import io.github.kryszak.gwatlin.config.BaseWiremockTest
 import io.kotest.assertions.assertSoftly
 import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 
@@ -12,10 +13,10 @@ internal class ColorClientTest : BaseWiremockTest() {
     private val colorClient = GWColorsClient()
 
     init {
-        should("Get dye colors") {
+        should("Get all dye colors") {
             // given
             val lang = ApiLanguage.EN
-            stubResponse("/v2/colors?ids=all", "/responses/gamemechanics/dye_colors.json", language = lang)
+            stubResponse("/v2/colors?ids=all", "/responses/gamemechanics/dye_colors_all.json", language = lang)
 
             // when
             val colors = colorClient.getAllDyeColors(lang)
@@ -36,6 +37,90 @@ internal class ColorClientTest : BaseWiremockTest() {
                 fur.shouldNotBeNull()
                 item shouldBe 20358
                 categories shouldContainExactly listOf("Gray", "Metal", "Rare")
+            }
+        }
+        should("Get color ids") {
+            // given
+            stubResponse("/v2/colors", "/responses/gamemechanics/dye_color_ids.json")
+
+            // when
+            val colorIds = colorClient.getDyeColorIds()
+
+            // then
+            colorIds shouldHaveSize 643
+        }
+        should("Get dye color") {
+            // given
+            val colorId = 5
+            stubResponse("/v2/colors/$colorId", "/responses/gamemechanics/dye_color.json")
+
+            // when
+            val color = colorClient.getDyeColor(colorId)
+
+            // then
+            assertSoftly(color) {
+                id shouldBe colorId
+                name shouldBe "Pitch"
+                baseRgb shouldContainExactly listOf(128, 26, 26)
+                assertSoftly(cloth) {
+                    brightness shouldBe -8
+                    contrast shouldBe 1
+                    hue shouldBe 275
+                    saturation shouldBe 0.0234375
+                    lightness shouldBe 1.09375
+                    rgb shouldContainExactly listOf(48, 46, 49)
+                }
+                assertSoftly(leather) {
+                    brightness shouldBe -8
+                    contrast shouldBe 1
+                    hue shouldBe 275
+                    saturation shouldBe 0.0234375
+                    lightness shouldBe 1.09375
+                    rgb shouldContainExactly listOf(48, 46, 49)
+                }
+                assertSoftly(metal) {
+                    brightness shouldBe -8
+                    contrast shouldBe 1.09375
+                    hue shouldBe 275
+                    saturation shouldBe 0.0234375
+                    lightness shouldBe 1.17188
+                    rgb shouldContainExactly listOf(44, 42, 45)
+                }
+                assertSoftly(fur!!) {
+                    brightness shouldBe -8
+                    contrast shouldBe 1
+                    hue shouldBe 275
+                    saturation shouldBe 0.0234375
+                    lightness shouldBe 1.09375
+                    rgb shouldContainExactly listOf(48, 46, 49)
+                }
+                item shouldBe 20366
+                categories shouldContainExactly listOf("Gray", "Metal", "Uncommon")
+            }
+        }
+        should("Get dye colors") {
+            // given
+            val ids = listOf(1,2,3)
+            stubResponse("/v2/colors?ids=${ids.joinToString(",")}", "/responses/gamemechanics/dye_colors.json")
+
+            // when
+            val colors = colorClient.getDyeColors(ids)
+
+            // then
+            assertSoftly(colors) {
+                it shouldHaveSize 3
+                assertSoftly(it[0]) {
+                    id shouldBe ids[0]
+                    name shouldBe "Dye Remover"
+                }
+                assertSoftly(it[1]) {
+                    id shouldBe ids[1]
+                    name shouldBe "Black"
+                }
+                assertSoftly(it[2]) {
+                    id shouldBe ids[2]
+                    name shouldBe "Chalk"
+                }
             }
         }
     }
