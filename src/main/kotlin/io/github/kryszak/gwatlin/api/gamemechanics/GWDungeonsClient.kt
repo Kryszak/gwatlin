a4537508656cd9f7d0e08302150fbd6e5ddb23b2
Kryszak <13157331+Kryszak@users.noreply.github.com>
2025-01-21 10:23:38 +0100

    205 add missing methods for GWDungeonsClient (#214)

--- src/main/kotlin/io/github/kryszak/gwatlin/api/gamemechanics/GWDungeonsClient.kt
+++ src/main/kotlin/io/github/kryszak/gwatlin/api/gamemechanics/GWDungeonsClient.kt
@@ -14,10 +14,36 @@ class GWDungeonsClient {
 
     /**
      * Returns details about each dungeon and it's associated paths
+     * @param language language for response
      * Documentation can be found in the [GW2 Wiki](https://wiki.guildwars2.com/wiki/API:2/account/dungeons)
      */
     @JvmOverloads
-    fun getDungeons(language: ApiLanguage? = null): List<Dungeon> {
+    fun getAllDungeons(language: ApiLanguage? = null): List<Dungeon> {
         return dungeonsClient.getDungeons(language)
+    }
+
+    /**
+     * Returns list of all dungeon ids
+     */
+    fun getDungeonIds(): List<String> {
+        return dungeonsClient.getDungeonIds()
+    }
+
+    /**
+     * Returns dungeon for given id
+     * @param id of dungeon
+     * @param language language for response
+     */
+    fun getDungeon(id: String, language: ApiLanguage? = null): Dungeon {
+        return dungeonsClient.getDungeon(id, language)
+    }
+
+    /**
+     * Returns dungeons for given id list
+     * @param ids of dungeons
+     * @param language language for response
+     */
+    fun getDungeons(ids: List<String>, language: ApiLanguage? = null): List<Dungeon> {
+        return dungeonsClient.getDungeons(ids, language)
     }
 }
