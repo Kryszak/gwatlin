2d6ab8916b6fd10cf4e911254ca4a1fcb89ec237
Kryszak <prajskrzysztof@gmail.com>
2025-01-22 12:27:22 +0100

    207 add missing methods for GWTitlesClient

--- src/main/kotlin/io/github/kryszak/gwatlin/clients/gamemechanics/TitleClient.kt
+++ src/main/kotlin/io/github/kryszak/gwatlin/clients/gamemechanics/TitleClient.kt
@@ -1,5 +1,6 @@
 package io.github.kryszak.gwatlin.clients.gamemechanics
 
+import io.github.kryszak.gwatlin.api.ApiLanguage
 import io.github.kryszak.gwatlin.api.gamemechanics.model.Title
 import io.github.kryszak.gwatlin.http.BaseHttpClient
 
@@ -7,7 +8,20 @@ internal class TitleClient: BaseHttpClient() {
 
     private val titlesEndpoint = "/titles"
 
-    fun getTitles(language: io.github.kryszak.gwatlin.api.ApiLanguage?): List<Title> {
+    fun getAllTitles(language: ApiLanguage?): List<Title> {
         return getRequest("$titlesEndpoint?ids=all", language)
+    }
+
+    fun getTitleIds(): List<Int> {
+        return getRequest(titlesEndpoint)
+    }
+
+    fun getTitle(id: Int, language: ApiLanguage?): Title {
+        return getRequest("$titlesEndpoint/$id", language)
+    }
+
+    fun getTitles(ids: List<Int>, language: ApiLanguage?): List<Title> {
+        val params = ids.joinToString(",")
+        return getRequest("$titlesEndpoint?ids=$params", language)
     }
 }
