eeb79aafa01644285a10393a80a487aa9a499ad8
Kryszak <prajskrzysztof@gmail.com>
2025-06-30 11:38:37 +0200

    261 fix pvp season without start and end date

--- gwatlin/src/main/kotlin/io/github/kryszak/gwatlin/api/pvp/model/season/PvpSeason.kt
+++ gwatlin/src/main/kotlin/io/github/kryszak/gwatlin/api/pvp/model/season/PvpSeason.kt
@@ -1,17 +1,21 @@
 package io.github.kryszak.gwatlin.api.pvp.model.season
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import java.time.OffsetDateTime
 
 /**
  * Data model for pvp season object
  */
 @Serializable
 data class PvpSeason(
-        val id: String,
-        val name: String,
-        val start: String,
-        val end: String,
-        val active: Boolean,
-        val divisions: List<PvpSeasonDivision> = listOf(),
-        val leaderboards: PvpSeasonLeaderboard
+    val id: String,
+    val name: String,
+    @Contextual
+    val start: OffsetDateTime? = null,
+    @Contextual
+    val end: OffsetDateTime? = null,
+    val active: Boolean,
+    val divisions: List<PvpSeasonDivision> = listOf(),
+    val leaderboards: PvpSeasonLeaderboard,
 )
